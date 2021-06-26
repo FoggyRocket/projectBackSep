7cc70d19411c6370dafb64d36f1598fa89da2ede
Dylan Torres <foggyrocket@MacBook-Pro-de-Dylan.local>
2021-06-26 12:25:17 -0500

    init project config

---
+++ app.js
@@ -0,0 +1,53 @@
+require("dotenv").config()//<=== importmos el dot env
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+//importamos mongoose y el dotenv
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+
+//Agregamos la conexion  de mongoose  
+mongoose.connect(process.env.DB,{
+    useUnifiedTopology:true
+})
+.then((x)=>{
+    console.log(`Connect to Mongo! Database name: "${x.connections[0].name}"`)
+}).catch((err)=>{
+    console.log("Error connecting to mongo", err)
+})
+
+
+
+
+const app = express();
+//utilizamos cors para darle permisos a otras apps
+
+app.use(
+        cors({
+            origin:["http:localhost:3000","https://www.paginaDeploy.com"],
+            credentials:true
+        })
+    );
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//estas son las rutass
+//por practica le  agregamos prefijo api
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+//app.use('/api/proucts',producRouter)
+
+//una ueva ruta que tome por  defecto cuando refresquemo
+
+module.exports = app;
